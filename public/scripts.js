9280c701eff2778d28b0098586175785d99f785b
Fabio Roberto <fabiorvs@globo.com>
2020-07-20 15:29:12 -0300

    Chat

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-var urlChat = 'https://fabiochat.herokuapp.com:3000/';
+var urlChat = 'https://fabiochat.herokuapp.com/';
 var socket = io(urlChat);
 
 function renderMessage(message) {
--- server.js
+++ server.js
@@ -39,4 +39,4 @@ io.on('connection', socket => {
     });
 })
 
-server.listen(3000);
+server.listen(80);

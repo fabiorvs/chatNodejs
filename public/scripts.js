ee7d85ba9b99e4962e9f0d9235a19011b6b64d04
Fabio Roberto <fabiorvs@globo.com>
2020-07-20 14:50:07 -0300

    Chat

---
+++ public/scripts.js
@@ -0,0 +1,35 @@
+var urlChat = 'http://localhost:3000';
+var socket = io(urlChat);
+
+function renderMessage(message) {
+    $('.messages').append('<div class="message"><strong>' + message.author + '</strong>:' + message.message + '</div>');
+}
+
+socket.on('previousMessages', function (messages) {
+    for (message of messages) {
+        renderMessage(message);
+    }
+});
+
+socket.on('receivedMessage', function (message) {
+    renderMessage(message);
+});
+
+$('#chat').submit(function (event) {
+    event.preventDefault();
+    var author = $('input[name=username]').val();
+    var message = $('input[name=message]').val();
+
+    if (author.length && message.length) {
+        var messageObject = {
+            author: author,
+            message: message,
+        };
+    }
+
+    renderMessage(messageObject);
+
+    socket.emit('sendMessage', messageObject);
+
+    $('input[name=message]').val('');
+});
